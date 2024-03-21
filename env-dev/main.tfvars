06fef8d35c14b084f6ed187229c923b6eea52804
Sumanth <9ssumanth@gmail.com>
2024-03-21 15:14:26 +0530

    creating subnet

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,5 +4,7 @@ project_name = "expense"
 vpc = {                             ## for each
   main = {                          ## key
     vpc_cidr = "10.10.0.0/21"       ##value
+      subnets_cidr = ["10.10.0.0/25", "10.10.0.128/25"]
+      az = ["us-east-1a", "us-east-1b"]
   }
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,13 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-${var.project_name}-vpc"
   }
 }
+
+resource "aws_subnet" "main" {
+  count      = length(var.subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet-${count.index}"
+  }
+}
