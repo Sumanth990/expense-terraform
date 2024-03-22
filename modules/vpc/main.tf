86bc05a0f78ed1e0def3703992544eb4d22765c4
Sumanth <9ssumanth@gmail.com>
2024-03-22 08:56:17 +0530

    Peering - auto_accept, name-tag

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -21,5 +21,9 @@ resource "aws_vpc_peering_connection" "main" {
   #peer_owner_id = var.peer_owner_id # optional - In our case both the vpc are in same account, so not required
   peer_vpc_id   = data.aws_vpc.default.id #we tried to achieve one peering connection.
   vpc_id        = aws_vpc.main.id #We tried to fetch the existing default VPC info rather than hard coding it.
+  auto_accept   = true
 
+  tags = {
+    Name = "${var.env}-vpc-with-default-vpc"
+  }
 }
