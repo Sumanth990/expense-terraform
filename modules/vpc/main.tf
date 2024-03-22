5ba94a4cadfba23dcad82540cdb76031f237eedd
Sumanth <9ssumanth@gmail.com>
2024-03-22 22:14:42 +0530

    creating 4 subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,9 +7,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count      = length(var.public_subnets_cidrsubnets_cidr)
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.public_subnets_cidrsubnets_cidr, count.index)
+  cidr_block = element(var.public_subnets_cidr, count.index)
   availability_zone = element(var.az, count.index)
 
   tags = {
@@ -18,9 +18,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count      = length(var.private_subnets_cidr.subnets_cidr)
+  count      = length(var.private_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.private_subnets_cidr.subnets_cidr, count.index)
+  cidr_block = element(var.private_subnets_cidr, count.index)
   availability_zone = element(var.az, count.index)
 
   tags = {
