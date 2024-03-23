3c2879351512da94ebb71f072e72331cccb0f356
Sumanth <9ssumanth@gmail.com>
2024-03-23 14:50:39 +0530

    created 3-tier application

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -76,19 +76,19 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
-  count      = length(var.private_subnets_cidr)
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.private_subnets_cidr, count.index)
+  cidr_block = element(var.web_subnets_cidr, count.index)
   availability_zone = element(var.az, count.index)
 
   tags = {
-    Name = "private-subnet-${count.index+1}"
+    Name = "web-subnet-${count.index+1}"
   }
 }
 
-  resource "aws_route_table" "private" {
-    count = length(var.private_subnets_cidr)
+  resource "aws_route_table" "web" {
+    count = length(var.web_subnets_cidr)
     vpc_id = aws_vpc.main.id
 
     route {
@@ -97,20 +97,128 @@ resource "aws_subnet" "private" {
     }
 
     route {
-      cidr_block                = data.aws_vpc.default.cidr_block # for default vpc we are creating peering connection
+      cidr_block                =  data.aws_vpc.default.cidr_block # for default vpc we are creating peering connection
       vpc_peering_connection_id = aws_vpc_peering_connection.main.id # before creating association we need to create peering connection
     }
 
     tags = {
-      Name = "private-rt-${count.index+1}"
+      Name = "web-rt-${count.index+1}"
     }
   }
 
-  resource "aws_route_table_association" "private" { # this route table will be associated to public subnet
-    count = length(var.private_subnets_cidr) # two route tables
-    route_table_id = lookup(element(aws_route_table.private,count.index), "id", null) #aws_route_table.private [count.index].id
-    subnet_id = lookup(element(aws_subnet.private,count.index), "id", null)
+  resource "aws_route_table_association" "web" { # this route table will be associated to public subnet
+    count = length(var.web_subnets_cidr) # two route tables
+    route_table_id = lookup(element(aws_route_table.web,count.index), "id", null) #aws_route_table.private [count.index].id
+    subnet_id = lookup(element(aws_subnet.web,count.index), "id", null)
   }
+
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.web_subnets_cidr, count.index)
+  availability_zone = element(var.az, count.index)
+
+  tags = {
+    Name = "web-subnet-${count.index+1}"
+  }
+}
+
+resource "aws_route_table" "web" {
+  count = length(var.web_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = lookup(element(aws_nat_gateway.main,count.index), "id", null)
+  }
+
+  route {
+    cidr_block                =  data.aws_vpc.default.cidr_block # for default vpc we are creating peering connection
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id # before creating association we need to create peering connection
+  }
+
+  tags = {
+    Name = "web-rt-${count.index+1}"
+  }
+}
+
+resource "aws_route_table_association" "web" { # this route table will be associated to public subnet
+  count = length(var.web_subnets_cidr) # two route tables
+  route_table_id = lookup(element(aws_route_table.web,count.index), "id", null) #aws_route_table.private [count.index].id
+  subnet_id = lookup(element(aws_subnet.web,count.index), "id", null)
+}
+
+resource "aws_subnet" "app" {
+  count      = length(var.app_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.app_subnets_cidr, count.index)
+  availability_zone = element(var.az, count.index)
+
+  tags = {
+    Name = "app-subnet-${count.index+1}"
+  }
+}
+
+resource "aws_route_table" "app" {
+  count = length(var.app_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = lookup(element(aws_nat_gateway.main,count.index), "id", null)
+  }
+
+  route {
+    cidr_block                =  data.aws_vpc.default.cidr_block # for default vpc we are creating peering connection
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id # before creating association we need to create peering connection
+  }
+
+  tags = {
+    Name = "app-rt-${count.index+1}"
+  }
+}
+
+resource "aws_route_table_association" "app" { # this route table will be associated to public subnet
+  count = length(var.app_subnets_cidr) # two route tables
+  route_table_id = lookup(element(aws_route_table.app,count.index), "id", null) #aws_route_table.private [count.index].id
+  subnet_id = lookup(element(aws_subnet.app,count.index), "id", null)
+}
+
+resource "aws_subnet" "db" {
+  count      = length(var.db_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.db_subnets_cidr, count.index)
+  availability_zone = element(var.az, count.index)
+
+  tags = {
+    Name = "db-subnet-${count.index+1}"
+  }
+}
+
+resource "aws_route_table" "db" {
+  count = length(var.db_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = lookup(element(aws_nat_gateway.main,count.index), "id", null)
+  }
+
+  route {
+    cidr_block                =  data.aws_vpc.default.cidr_block # for default vpc we are creating peering connection
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id # before creating association we need to create peering connection
+  }
+
+  tags = {
+    Name = "db-rt-${count.index+1}"
+  }
+}
+
+resource "aws_route_table_association" "db" { # this route table will be associated to public subnet
+  count = length(var.db_subnets_cidr) # two route tables
+  route_table_id = lookup(element(aws_route_table.db,count.index), "id", null) #aws_route_table.private [count.index].id
+  subnet_id = lookup(element(aws_subnet.db,count.index), "id", null)
+}
 
 resource "aws_route" "main" {
   route_table_id            = aws_vpc.main.main_route_table_id
@@ -122,47 +230,4 @@ resource "aws_route" "default-vpc" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = aws_vpc.main.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-}
-
-##testing purpose let's create a instance
-data "aws_ami" "example" {
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = ["973714476881"]
-}
-
-#terraform:resource/aws_security_group
-
-resource "aws_security_group" "test" {
-  name        = "test"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_this"
-  }
-}
-
-resource "aws_instance" "test" {
-  ami           = data.aws_ami.example.image_id
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.private[0].id #lookup(element(aws_subnet.main, 0), "id", null) created VPC under this subnet
-  security_groups = [aws_security_group.test.id]
-  #to open port 22 we need to create security group to others [] -> expecting in a list
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,8 @@
 variable "vpc_cidr" {}
 variable "env" {}
 variable "project_name" {}
-variable "private_subnets_cidr" {}
 variable "public_subnets_cidr" {}
+variable "web_subnets_cidr" {}
+variable "app_subnets_cidr" {}
+variable "db_subnets_cidr" {}
 variable "az" {}
