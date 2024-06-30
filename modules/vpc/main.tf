29c79ad997fa31f1ea10906ab73d1750161f8bcd
Sumanth <9ssumanth@gmail.com>
2024-06-30 21:39:42 +0530

    peering connection

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,6 +9,11 @@ resource "aws_subnet" "main" {
     Name = "subnet-${count.index}"
   }
 }
+#peering connection
+resource "aws_vpc_peering_connection" "main" {
+  peer_vpc_id   = data.aws_vpc.default.id
+  vpc_id        = aws_vpc.main.id
+}
 
 #security group #allow ports
 
