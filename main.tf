e5b66d559e1a0303093032301beba9e12e67195c
Sumanth <9ssumanth@gmail.com>
2024-06-30 21:02:12 +0530

    vpc - tag

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "vpc" {
   source     = "./modules/vpc"
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
+
+  env          = var.env
+  project_name = var.project_name
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,4 +5,8 @@
 #VPC
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
+
+  tags = {
+    Name = "${var.env}-${var.project_name}-vpc"
+  }
 }
