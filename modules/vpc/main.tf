39e811f00fd66058e11c0536188a0f9743f49dec
Sumanth <9ssumanth@gmail.com>
2024-07-01 08:12:51 +0530

    route

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,6 +13,24 @@ resource "aws_subnet" "main" {
 resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id   = data.aws_vpc.default.id
   vpc_id        = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-vpc-with-default-vpc"
+  }
+}
+
+#route entry in route table
+
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
 #security group #allow ports
