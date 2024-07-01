e8cd5ddaa6641537176cf963adb22f60809b9214
Sumanth <9ssumanth@gmail.com>
2024-07-01 11:55:15 +0530

    public route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -44,6 +44,30 @@ resource "aws_route" "default-vpc" {
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
+#GW
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.name}-igw"
+  }
+}
+
+#route table
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnet_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-rt-${count.index+1}"
+  }
+}
+
 #security group #allow ports
 
 #VPC
