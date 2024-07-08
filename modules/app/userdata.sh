d80893a8db173ea4f98a470c886e75b353fb355f
Sumanth <9ssumanth@gmail.com>
2024-07-08 22:24:17 +0530

    user_data

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,4 +2,4 @@
 
 yum install ansible python3.12-pip -y &>>/opt/userdata.log
 pip3.12 install botocore boto3 -y &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/Sumanth990/expense-ansible.git expense.yml -e role_name=${role_name}-e env=${env} &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Sumanth990/expense-ansible.git expense.yml -e role_name=${role_name} -e env=${env} &>>/opt/userdata.log
